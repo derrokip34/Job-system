43a3e59c2c5a489849d5f865c24b3e5c0675c60e
Derrick Kiprop <derrickip34@gmail.com>
2023-04-11 17:00:51 +0300

    create function to save job_seekers to database

---
+++ db_connection.py
@@ -0,0 +1,10 @@
+import psycopg2
+
+def save_to_db(data):
+    conn = psycopg2.connect(database='csc_227',user='postgres',host='localhost',port='5432',password='enkay2008')
+    cur = conn.cursor()
+    query = "INSERT INTO job_seekers (first_name,last_name,email,phone_num,gender,dob,category,area,password) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s);"
+    cur.execute(query, (data["first_name"],data["last_name"],data["email"],data["phone_no"],data["gender"],data["dob"],data["category"],data["area"],data["password"]))
+    conn.commit()
+    cur.close()
+    conn.close()
