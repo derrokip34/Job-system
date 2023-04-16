74beb7c78df91ec9daba20526592df095c811ada
Derrick Kiprop <derrickip34@gmail.com>
2023-04-16 13:51:19 +0300

    add a hash password function

---
+++ validate.py
@@ -0,0 +1,9 @@
+import bcrypt
+
+def hash_password(password):
+    salt = b'$2b$12$SJv9T2zvJFjI6bYtibhZv.'
+    new_pass_bytes = password.encode('utf-8')
+    new_hashed = bcrypt.hashpw(new_pass_bytes,salt)
+    hashed_password = new_hashed.decode('utf-8')
+    return hashed_password
+
